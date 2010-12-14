2f37d6cc9dafa6370deaa6b7e7017981a9b9f05f
intrigeri <intrigeri@boum.org>
2010-12-14 21:44:04 +0100

    Remove bashism.
    In POSIX, string equality is tested using '='.

--- manifests/definitions/config.pp
+++ manifests/definitions/config.pp
@@ -34,8 +34,8 @@ define postfix::config ($ensure = present, $value, $nonstandard = false) {
     present: {
       exec {"postconf -e ${name}='${value}'":
         unless  => $nonstandard ? {
-          false => "test \"x$(postconf -h ${name})\" == 'x${value}'",
-          true  => "test \"x$(egrep '^${name} ' /etc/postfix/main.cf | cut -d= -f2 | cut -d' ' -f2)\" == 'x${value}'",
+          false => "test \"x$(postconf -h ${name})\" = 'x${value}'",
+          true  => "test \"x$(egrep '^${name} ' /etc/postfix/main.cf | cut -d= -f2 | cut -d' ' -f2)\" = 'x${value}'",
         },
         notify  => Service["postfix"],
         require => File["/etc/postfix/main.cf"],
