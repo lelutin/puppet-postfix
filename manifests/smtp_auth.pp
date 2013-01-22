49f1024e1673c50c728d51a542c82b7a7cc4505b
varac <varacanero@zeromail.org>
2013-01-22 22:26:01 +0100

    added smtp_auth.pp

---
+++ manifests/smtp_auth.pp
@@ -0,0 +1,41 @@
+/*
+== Definition: postfix::smtp_auth
+
+Manages content of the /etc/postfix/smtp_auth map.
+
+Requires:
+- Class["postfix"]
+- Postfix::Hash["/etc/postfix/smtp_auth"]
+- common::line (from module common)
+
+Example usage:
+
+  node "toto.example.com" {
+
+    include postfix
+
+    postfix::hash { "/etc/postfix/smtp_auth":
+      ensure => present,
+    }
+    postfix::config { "smpt_auth_maps":
+      value => "hash:/etc/postfix/smtp_auth"
+    }
+    postfix::smtp_auth { "gmail.com":
+      ensure      => present,
+      user     => "USER",
+      password => "PW",
+    }
+  }
+*/
+
+
+define postfix::smtp_auth ($ensure="present", $user, $password) {
+  line {"${name}":
+    ensure => $ensure,
+    file   => "/etc/postfix/smtp_auth",
+    line   => "${name} ${user}:${password}",
+    notify => Exec["generate /etc/postfix/smtp_auth.db"],
+    require => Package["postfix"],
+  }
+
+}
